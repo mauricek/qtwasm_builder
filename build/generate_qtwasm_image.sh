2f2493176fca9473d3547b795827b2e2f298460a
Maurice Kalinowski <maurice.kalinowski@qt.io>
2019-07-24 11:13:12 +0200

    Make branch an optional parameter
    As multiple versions of Qt now support WebAssembly, images should be
    built for each branch. Default to 5.13 still as the latest stable
    release.

--- build/generate_qtwasm_image.sh
+++ build/generate_qtwasm_image.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 #set -ex
-
+targetBranch=${1:-5.13}
 dockerRepo=maukalinow/qtwasm_builder
-qtHead=$(git ls-remote git://code.qt.io/qt/qt5.git refs/heads/5.13 | cut -f 1)
+qtHead=$(git ls-remote git://code.qt.io/qt/qt5.git refs/heads/$targetBranch | cut -f 1)
 
-echo "*** Checking Qt HEAD: $qtHead"
+echo "*** Checking Qt HEAD: $qtHead in branch $targetBranch"
 
-if docker pull $dockerRepo:$qtHead
+if docker pull $dockerRepo:${targetBranch}_$qtHead
 then
-    echo "*** Image for qt5.git branch 5.13 $qtHead already generated. Nothing to do..."
+    echo "*** Image for qt5.git branch $targetBranch $qtHead already generated. Nothing to do..."
     exit 0
 fi
 
-if ! docker build --no-cache -t qtwasm_builder .
+
+if ! docker build --no-cache -t qtwasm_builder_$targetBranch --build-arg targetBranch=$targetBranch .
 then
    echo "*** Could not build image! Check output"
    exit 1
 fi
 
-docker tag qtwasm_builder $dockerRepo:$qtHead
-docker tag qtwasm_builder $dockerRepo:latest
+docker tag qtwasm_builder_$targetBranch $dockerRepo:${targetBranch}_$qtHead
+docker tag qtwasm_builder_$targetBranch $dockerRepo:${targetBranch}_latest
 
 echo "*** Image generated and locally tagged, now push"
-echo "    docker push $dockerRepo:$qtHead"
-echo "    docker push $dockerRepo:latest"
+echo "    docker push $dockerRepo:${targetBranch}_$qtHead"
+echo "    docker push $dockerRepo:${targetBranch}_latest"
 
 exit 0
