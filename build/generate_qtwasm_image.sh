b5288b61c4d0f7b1a346b5847744dbde887f0221
Maurice Kalinowski <maurice.kalinowski@qt.io>
2019-07-30 09:27:18 +0200

    Switch to working configuration
    The current configuration of branches is able to compile
    applications properly with some cache for speeding linking
    up further.
    
    Running applications currently does not work. This is mostly
    likely related to
    https://github.com/emscripten-core/emscripten/pull/7977
    according to the debug output of the application itself.

--- build/generate_qtwasm_image.sh
+++ build/generate_qtwasm_image.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 #set -ex
-targetBranch=${1:-5.13}
-dockerRepo=maukalinow/qtwasm_builder
+targetBranch=${1:-dev}
+dockerRepo=maukalinow/qtwasm_object_files_builder
 qtHead=$(git ls-remote git://code.qt.io/qt/qt5.git refs/heads/$targetBranch | cut -f 1)
 
 echo "*** Checking Qt HEAD: $qtHead in branch $targetBranch"
 
-#if docker pull $dockerRepo:${targetBranch}_$qtHead
-#then
-#    echo "*** Image for qt5.git branch $targetBranch $qtHead already generated. Nothing to do..."
-#    exit 0
-#fi
+if docker pull $dockerRepo:${targetBranch}_$qtHead
+then
+    echo "*** Image for qt5.git branch $targetBranch $qtHead already generated. Nothing to do..."
+    exit 0
+fi
 
-
-if ! docker build --no-cache -t qtwasm_builder_$targetBranch --build-arg targetBranch=$targetBranch .
+if ! docker build --no-cache -t qtwasm_object_files_builder_$targetBranch --build-arg targetBranch=$targetBranch .
 then
    echo "*** Could not build image! Check output"
    exit 1
 fi
 
-docker tag qtwasm_builder_$targetBranch $dockerRepo:${targetBranch}_$qtHead
-docker tag qtwasm_builder_$targetBranch $dockerRepo:${targetBranch}_latest
+docker tag qtwasm_object_files_builder_$targetBranch $dockerRepo:${targetBranch}_$qtHead
+docker tag qtwasm_object_files_builder_$targetBranch $dockerRepo:${targetBranch}_latest
 
 echo "*** Image generated and locally tagged, now push"
 echo "    docker push $dockerRepo:${targetBranch}_$qtHead"
