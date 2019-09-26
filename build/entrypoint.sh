ad38fd1b36f278bcebfd0a918ec5ec49489bcd08
Alexandre Tolstenko <tolstenko@gmail.com>
2019-09-26 14:02:07 -0300

    getting username via docker login

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -f "/project/source/CMakeLists.txt" ]; then
-    cmake -H/project/source -B/project/build & cmake --build /project/build -j `grep -c '^processor' /proc/cpuinfo`
+    cmake -H/project/source -B/project/build & cmake --build /project/build --parallel `grep -c '^processor' /proc/cpuinfo`
 else
     qmake /project/source && make -j `grep -c '^processor' /proc/cpuinfo`
 fi
--- build/generate_qtwasm_image.sh
+++ build/generate_qtwasm_image.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #set -ex
-targetBranch=${1:-5.13}
-dockerRepo=maukalinow/qtwasm_builder
-qtHead=$(git ls-remote https://code.qt.io/cgit/qt/qt5.git refs/heads/$targetBranch | cut -f 1)
+targetBranch=${1:-5.13.1}
+username=$(docker info | sed '/Username:/!d;s/.* //');
+dockerRepo=${username}/qtwasm
+qtHead=$(git ls-remote https://github.com/qt/qt5.git refs/heads/$targetBranch | cut -f 1)
 
+# get and set the name of the latest version of emscripten from trzeci
 echo "*** Checking Qt HEAD: $qtHead in branch $targetBranch"
 
 if docker pull $dockerRepo:${targetBranch}_$qtHead
@@ -13,17 +15,19 @@ then
 fi
 
 
-if ! docker build --no-cache -t qtwasm_builder_$targetBranch --build-arg targetBranch=$targetBranch .
+if ! docker build -t qtwasm_builder_$targetBranch --build-arg targetBranch=$targetBranch .
 then
    echo "*** Could not build image! Check output"
    exit 1
 fi
 
 docker tag qtwasm_builder_$targetBranch $dockerRepo:${targetBranch}_$qtHead
-docker tag qtwasm_builder_$targetBranch $dockerRepo:${targetBranch}_latest
+docker tag qtwasm_builder_$targetBranch $dockerRepo:${targetBranch}
+docker tag qtwasm_builder_$targetBranch $dockerRepo:latest
 
 echo "*** Image generated and locally tagged, now push"
 echo "    docker push $dockerRepo:${targetBranch}_$qtHead"
-echo "    docker push $dockerRepo:${targetBranch}_latest"
+echo "    docker push $dockerRepo:${targetBranch}"
+echo "    docker push $dockerRepo:latest"
 
 exit 0
